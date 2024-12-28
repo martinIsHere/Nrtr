82a7dbf08a142ed286b60393d987df1bdd7eec0a
martinIsHere <marty.havardsen@gmail.com>
2024-12-28 14:14:55 +0100

    commit before attempting to fix collision bug for dynamic sizes

--- src/CollisionComponent.cpp
+++ src/CollisionComponent.cpp
@@ -46,7 +46,7 @@ inline bool CollisionComponent::apply_correction_in_LEFT_COLLISION(int oldYPos, 
 		)
 		|| m_gameMap->getState(
 			int((m_posComp->getx() + COLSN_OFST + hitBoxOffsetX) / TILE_SIZE_PIXELS),
-			int(((oldYPos + TILE_SIZE_PIXELS - COLSN_OFST - 1) / TILE_SIZE_PIXELS)))
+			int(((oldYPos + AVERAGE_ENTITY_SIZE_PIXELS - COLSN_OFST - 1) / TILE_SIZE_PIXELS)))
 		) {
 
 		// Correct collision: move back to the left boundary
@@ -63,11 +63,11 @@ inline bool CollisionComponent::apply_correction_in_LEFT_COLLISION(int oldYPos, 
 
 inline bool CollisionComponent::apply_correction_in_RIGHT_COLLISION(int oldYPos, int hitBoxOffsetX, int hitBoxTopOffset) {
 	if (m_gameMap->getState(
-		int(((m_posComp->getx() + TILE_SIZE_PIXELS - COLSN_OFST - hitBoxOffsetX - 1) / TILE_SIZE_PIXELS)),
+		int(((m_posComp->getx() + AVERAGE_ENTITY_SIZE_PIXELS - COLSN_OFST - hitBoxOffsetX - 1) / TILE_SIZE_PIXELS)),
 		int((oldYPos + COLSN_OFST + hitBoxTopOffset) / TILE_SIZE_PIXELS))
 		|| m_gameMap->getState(
-			int(((m_posComp->getx() + TILE_SIZE_PIXELS - COLSN_OFST - hitBoxOffsetX - 1) / TILE_SIZE_PIXELS)),
-			int(((oldYPos + TILE_SIZE_PIXELS - COLSN_OFST - 1) / TILE_SIZE_PIXELS)))) {
+			int(((m_posComp->getx() + AVERAGE_ENTITY_SIZE_PIXELS - COLSN_OFST - hitBoxOffsetX - 1) / TILE_SIZE_PIXELS)),
+			int(((oldYPos + AVERAGE_ENTITY_SIZE_PIXELS - COLSN_OFST - 1) / TILE_SIZE_PIXELS)))) {
 
 		// Correct collision: move back to the right boundary
 		m_posComp->getx() =
@@ -87,7 +87,7 @@ inline bool CollisionComponent::apply_correction_in_ABOVE_COLLISION(int hitBoxOf
 		int((m_posComp->getx() + COLSN_OFST + hitBoxOffsetX) / TILE_SIZE_PIXELS),
 		int((m_posComp->gety() + COLSN_OFST + hitBoxTopOffset) / TILE_SIZE_PIXELS))
 		|| m_gameMap->getState(
-			int(((m_posComp->getx() + TILE_SIZE_PIXELS - COLSN_OFST - hitBoxOffsetX - 1) / TILE_SIZE_PIXELS)),
+			int(((m_posComp->getx() + AVERAGE_ENTITY_SIZE_PIXELS - COLSN_OFST - hitBoxOffsetX - 1) / TILE_SIZE_PIXELS)),
 			int((m_posComp->gety() + COLSN_OFST + hitBoxTopOffset) / TILE_SIZE_PIXELS))) {
 
 		// Correct collision: move back to the top boundary
@@ -106,10 +106,10 @@ inline bool CollisionComponent::apply_correction_in_ABOVE_COLLISION(int hitBoxOf
 inline bool CollisionComponent::apply_correction_in_BELOW_COLLISION(int hitBoxOffsetX, int hitBoxTopOffset) {
 	if (m_gameMap->getState(
 		int((m_posComp->getx() + COLSN_OFST + hitBoxOffsetX) / TILE_SIZE_PIXELS),
-		int((m_posComp->gety() + TILE_SIZE_PIXELS - COLSN_OFST - 1) / TILE_SIZE_PIXELS))
+		int((m_posComp->gety() + AVERAGE_ENTITY_SIZE_PIXELS - COLSN_OFST - 1) / TILE_SIZE_PIXELS))
 		|| m_gameMap->getState(
-			int(((m_posComp->getx() + TILE_SIZE_PIXELS - COLSN_OFST - hitBoxOffsetX - 1) / TILE_SIZE_PIXELS)),
-			int((m_posComp->gety() + TILE_SIZE_PIXELS - COLSN_OFST - 1) / TILE_SIZE_PIXELS))) {
+			int(((m_posComp->getx() + AVERAGE_ENTITY_SIZE_PIXELS - COLSN_OFST - hitBoxOffsetX - 1) / TILE_SIZE_PIXELS)),
+			int((m_posComp->gety() + AVERAGE_ENTITY_SIZE_PIXELS - COLSN_OFST - 1) / TILE_SIZE_PIXELS))) {
 
 		// Correct collision: move back to the bottom boundary
 		m_posComp->gety() =
@@ -128,11 +128,15 @@ bool CollisionComponent::correct_possible_collision() {
 
 	bool collision = false;
 
+	// may act up in the future... idk we'll see ------------------------------------------------------------
+	// (int)m_posComp->getVely() is sketchy
 	int oldYPos = int(m_posComp->gety() - (int)m_posComp->getVely());
 
 	// Offsets for hitbox
-	static int hitBoxOffsetX = int(TILE_SIZE_PIXELS * 0.25f);
-	static int hitBoxTopOffset = int(TILE_SIZE_PIXELS * 0.5f);
+	static int hitBoxOffsetX = int(AVERAGE_ENTITY_SIZE_PIXELS * 0.25f);
+	static int hitBoxTopOffset = int(AVERAGE_ENTITY_SIZE_PIXELS * 0.5f);
+
+	// TODO: check for collision with edges of map
 
 	// X-axis collision handling
 	if (m_posComp->isMovingVelX()) {
